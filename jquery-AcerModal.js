eca3402604058b5a42acbad498ef841d87f66c99
victor-tsai <princeoftennis735@gmail.com>
2021-05-19 10:19:03 +0800

    新增form彈窗refresh

--- jquery-AcerModal.js
+++ jquery-AcerModal.js
@@ -158,7 +158,42 @@ $.fn.acerModal = function acerModal (option){
             }
             $this.$modalBody.append($select);
         }else if(type == 'file'){
-
+            $this.$modalBody.empty();
+            let $form = $("<form id='uploadForm'></form>");
+            if(data instanceof Array){
+                data.forEach(function(obj){
+                    let $input = $("<input class='form-control'></input>");
+                    let $div = $('<div class="form-group row"></div>');
+                    let $colSm = $("<div class='col-sm-10'></div>")
+                    let $label = $('<label></label>');
+                    $label.attr('for',obj.path);
+                    $input.attr('type',obj.type);
+                    $input.attr('name',obj.path);
+                    $label.text(obj.label);
+                    $label.addClass("col-sm-10");
+                    if(obj.value != null){
+                        $input.attr('value',obj.value);
+                    }
+                    if(obj.type != 'hidden'){
+                        $colSm.append($input);
+                        $div.append($label,$colSm)
+                        $form.append($div)
+                    }else{
+                        $form.append($input)
+                    }    
+                });
+            }
+            submit = $("<button class='btn btn-primary' type='button'></button>").text("上傳檔案");
+			submit.off().on('click',function(){
+				if(typeof callback ==="function" && callback != null){
+					callback('test');
+				}
+				console.log('callback function');
+                $this.close($this.ele);
+                clear($this);
+			})
+			$form.append(submit);
+			$this.$modalBody.append($form)
         }
 
     }
